620ea400b3fea9040aac01e97c6fe2e514b5ffdc
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-02-22 15:41:59 +0530

    Concurrency support

--- src/main/java/org/am/rate_limiting/FixedWindow.java
+++ src/main/java/org/am/rate_limiting/FixedWindow.java
@@ -2,12 +2,15 @@ package org.am.rate_limiting;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class FixedWindow {
 
     private final Map<ClientId, Window> windowByClientId = new HashMap<>();
     private final int windowSizeMs;
     private final int maxRequests;
+    private final Lock lock = new ReentrantLock(true);
 
     public FixedWindow(int windowSizeMs, int maxRequests) {
         this.windowSizeMs = windowSizeMs;
@@ -15,6 +18,16 @@ public class FixedWindow {
     }
 
     public boolean allowRequest(ClientId clientId, Timestamp requestTimestamp) {
+        try {
+            // Since the request if user based don't expect too much contention
+            lock.lock();
+            return synchronizedCheck(clientId, requestTimestamp);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private boolean synchronizedCheck(ClientId clientId, Timestamp requestTimestamp) {
         Window window = windowByClientId.get(clientId);
         if (window == null || requestTimestamp.epochMilli() > window.startTime.epochMilli() + windowSizeMs) {
             window = new Window(requestTimestamp);
--- src/main/java/org/am/rate_limiting/SlidingWindow.java
+++ src/main/java/org/am/rate_limiting/SlidingWindow.java
@@ -2,6 +2,8 @@ package org.am.rate_limiting;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class SlidingWindow {
 
@@ -9,19 +11,31 @@ public class SlidingWindow {
     private final int windowSizeMs;
     private final int maxRequests;
 
+    private final Lock lock = new ReentrantLock(true);
+
     public SlidingWindow(int windowSizeMs, int maxRequests) {
         this.windowSizeMs = windowSizeMs;
         this.maxRequests = maxRequests;
     }
 
     public boolean allowRequest(ClientId clientId, Timestamp requestTimestamp) {
+        try {
+            // Since the request if user based don't expect too much contention
+            lock.lock();
+            return synchronizedCheck(clientId, requestTimestamp);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean synchronizedCheck(ClientId clientId, Timestamp requestTimestamp) {
         Window window = windowByClientId.get(clientId);
         if (window == null || requestTimestamp.epochMilli() > window.startTime.epochMilli() + windowSizeMs) {
             window = new Window(requestTimestamp);
             windowByClientId.put(clientId, window);
         }
         // Determine available capacity based on how much of the window has passed
-        // Now
+        // Allow or disallow based on that capacity
         double proportionOfWindowPassed = (double) (requestTimestamp.epochMilli() - window.startTime.epochMilli()) / windowSizeMs;
         if (window.count + 1 <= Math.round(maxRequests - (maxRequests * proportionOfWindowPassed))) {
             window.count++;
