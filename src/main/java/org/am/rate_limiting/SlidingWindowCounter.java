13e8c7015d9aaa185c29c6977b9595451c7de688
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-02-28 15:04:11 +0530

    Fixed logic

--- src/main/java/org/am/rate_limiting/SlidingWindowCounter.java
+++ src/main/java/org/am/rate_limiting/SlidingWindowCounter.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class SlidingWindow implements RateLimiter {
+public class SlidingWindowCounter implements RateLimiter {
 
     private final Map<Client, Window> windowByClientId = new HashMap<>();
     private final int windowSizeMs;
@@ -13,7 +13,7 @@ public class SlidingWindow implements RateLimiter {
 
     private final Lock lock = new ReentrantLock(true);
 
-    public SlidingWindow(int windowSizeMs, int maxRequests) {
+    public SlidingWindowCounter(int windowSizeMs, int maxRequests) {
         this.windowSizeMs = windowSizeMs;
         this.maxRequests = maxRequests;
     }
@@ -31,14 +31,25 @@ public class SlidingWindow implements RateLimiter {
 
     public boolean synchronizedCheck(Client client, Timestamp requestTimestamp) {
         Window window = windowByClientId.get(client);
-        if (window == null || requestTimestamp.epochMilli() > window.startTime.epochMilli() + windowSizeMs) {
+        if (window == null) { // no previous window
             window = new Window(requestTimestamp);
             windowByClientId.put(client, window);
+        } else if (requestTimestamp.epochMilli() > window.startTime.epochMilli() + windowSizeMs) { // not in current window
+            if (requestTimestamp.epochMilli() <= window.startTime.epochMilli() + windowSizeMs + windowSizeMs) { // but has previous window
+                int prevCounter = window.prevCounter;
+                window = new Window(requestTimestamp);
+                window.prevCounter = prevCounter;
+                windowByClientId.put(client, window);
+            } else {
+                window = new Window(requestTimestamp);
+                windowByClientId.put(client, new Window(requestTimestamp));
+            }
         }
         // Determine available capacity based on how much of the window has passed
         // Allow or disallow based on that capacity
         double proportionOfWindowPassed = (double) (requestTimestamp.epochMilli() - window.startTime.epochMilli()) / windowSizeMs;
-        if (window.count + 1 <= Math.round(maxRequests - (maxRequests * proportionOfWindowPassed))) {
+        double previousWindowProportion = 1 - proportionOfWindowPassed;
+        if (maxRequests >= previousWindowProportion * window.prevCounter + window.count + 1) {
             window.count++;
             return true;
         }
@@ -49,9 +60,12 @@ public class SlidingWindow implements RateLimiter {
         Timestamp startTime;
         int count;
 
+        int prevCounter;
+
         public Window(Timestamp timestamp) {
             this.startTime = timestamp;
             count = 0;
+            prevCounter = 0;
         }
     }
 }
--- src/test/java/org/am/rate_limiting/SlidingWindowCounterTest.java
+++ src/test/java/org/am/rate_limiting/SlidingWindowCounterTest.java
@@ -4,23 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SlidingWindowTest {
+class SlidingWindowCounterTest {
 
     @Test
     void isRequestAllowed() {
-        RateLimiter rateLimiter = new SlidingWindow(5000, 3);
+        RateLimiter rateLimiter = new SlidingWindowCounter(5000, 3);
         Client user1Client = new Client("user1");
         long baseEpochMilli = 1708583924641L;
         assertTrue(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 1000)));
         assertTrue(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 3000)));
-        assertFalse(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 4000)));
+        assertTrue(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 4000)));
         assertFalse(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 4500)));
         assertFalse(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 4800)));
         assertFalse(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 5000)));
         assertFalse(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 6000)));
         assertTrue(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 7000)));
         assertTrue(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 8000)));
-        assertFalse(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 9000)));
+        assertTrue(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 9000)));
         assertFalse(rateLimiter.allowRequest(user1Client, new Timestamp(baseEpochMilli + 10000)));
     }
 
