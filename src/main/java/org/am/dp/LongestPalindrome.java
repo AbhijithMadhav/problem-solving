435f062838ad3b7fcaedc9932b97957e26f1dc94
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-03-07 14:44:17 +0530

    Longest palindrome

---
+++ src/main/java/org/am/dp/LongestPalindrome.java
@@ -0,0 +1,31 @@
+package org.am.dp;
+
+/**
+ * Two points to note
+ * 1. Checking for palindrome from middle is more easy
+ * 2. Look out for even and odd length palindromes
+ */
+public class LongestPalindrome {
+
+    public String longestPalindrome(String s) {
+        var res = "";
+        for (int i = 0; i < s.length(); i++) {
+            // odd length palindromes
+            res = checkForLongerPalindrome(s, i, i, res);
+
+            // even length palindrome
+            res = checkForLongerPalindrome(s, i, i+1, res);
+        }
+        return res;
+    }
+
+    private static String checkForLongerPalindrome(String s, int l, int r, String res) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r) ) {
+            if (r - l + 1 > res.length())
+                res = s.substring(l, r + 1);
+            l--;
+            r++;
+        }
+        return res;
+    }
+}
---
+++ src/test/java/org/am/dp/LongestPalindromeTest.java
@@ -0,0 +1,23 @@
+package org.am.dp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestPalindromeTest {
+
+    @Test
+    void longestPalindromicSubstring() {
+
+        LongestPalindrome lps = new LongestPalindrome();
+        // even palindrome
+        assertEquals("asdffdsa", lps.longestPalindrome("asdfkjaasdffdsakjdf"));
+        assertEquals("asdffdsa", lps.longestPalindrome("asdffdsakjdf"));
+        assertEquals("asdffdsa", lps.longestPalindrome("asdfkjaasdffdsa"));
+        assertEquals("asdffdsa", lps.longestPalindrome("abasdfkjaasdffdsa"));
+
+        // odd palindrome
+        assertEquals("asdfdsa", lps.longestPalindrome("asdfkjaasdfdsakjdf"));
+        assertEquals("i", lps.longestPalindrome("ihfgalkjgadbv"));
+    }
+}
