beb69ae1546db6bdbd857bbf14a2f24a7936ef28
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-04-03 11:42:50 +0530

    Generate subsets

---
+++ src/main/java/org/am/greedy/SubSet.java
@@ -0,0 +1,36 @@
+package org.am.greedy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SubSet {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> subsets = new LinkedList<>();
+        subsets.addFirst(new LinkedList<>()); // Need empty set
+        for (int size = 1; size <= nums.length; size++) {
+            for (int startIndex = 0; startIndex < nums.length; startIndex++) {
+                subsets.addAll(subsets(nums, size, startIndex));
+            }
+        }
+        return subsets;
+    }
+
+    private List<List<Integer>> subsets(int[] nums, int subsetSize, int startIndex) {
+        if (subsetSize == 1) {
+            List<Integer> list = new LinkedList<>();
+            list.addFirst(nums[startIndex]);
+            List<List<Integer>> listList = new LinkedList<>();
+            listList.addFirst(list);
+            return listList;
+        }
+        List<List<Integer>> subsets = new LinkedList<>();
+        for (int i = startIndex + 1; i + subsetSize - 1 <= nums.length; i++) { // had difficult with this condition
+            subsets.addAll(subsets(nums, subsetSize - 1, i)); // general difficulty with visualizing this
+        }
+        for (List<Integer> subset : subsets) {
+            subset.addFirst(nums[startIndex]); // general difficulty with visualizing this
+        }
+        return subsets;
+    }
+}
---
+++ src/test/java/org/am/greedy/SubSetTest.java
@@ -0,0 +1,23 @@
+package org.am.greedy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SubSetTest {
+
+    @Test
+    void subsets() {
+
+        assertThat(new SubSet().subsets(new int[] {1, 2, 3})).hasSameElementsAs(
+                List.of(
+                        List.of(),
+                        List.of(1), List.of(2), List.of(3),
+                        List.of(1, 2), List.of(1, 3), List.of(2, 3),
+                        List.of(1, 2, 3)
+                )
+        );
+    }
+}
