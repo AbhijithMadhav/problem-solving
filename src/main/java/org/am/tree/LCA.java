c24d9a72f16a276dd9380f1f7e9a856d633862a5
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-04-11 11:59:36 +0530

    Least common ancestor

---
+++ src/main/java/org/am/tree/LCA.java
@@ -0,0 +1,17 @@
+package org.am.tree;
+
+public class LCA {
+
+    // LCA is the node where p and q have to split on the back of the fact that the tree is a BST
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val <= root.val && q.val >= root.val || q.val <= root.val && p.val >= root.val)
+                break;
+            if (p.val < root.val)
+                root = root.left;
+            else
+                root = root.right;
+        }
+        return root;
+    }
+}
