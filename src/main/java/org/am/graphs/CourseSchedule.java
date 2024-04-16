9662109e7d6d653dce0325c24e1120ecda34829b
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-04-16 13:31:57 +0530

    Course Schedule

--- src/main/java/org/am/graphs/CourseSchedule.java
+++ src/main/java/org/am/graphs/CourseSchedule.java
@@ -1,6 +1,7 @@
 package org.am.graphs;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
@@ -10,41 +11,54 @@ import java.util.*;
  * Return true if you can finish all courses. Otherwise, return false.
  */
 public class CourseSchedule {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> adjMap = new HashMap<>();
 
+    private final Set<Integer> visited = new HashSet<>();
+    private final Set<Integer> onStack = new HashSet<>();
+    private final Deque<Integer> reversePostOrder = new LinkedList<>();
+    private final Map<Integer, List<Integer>> adjMap = new HashMap<>();
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+        if (canFinish(numCourses, prerequisites)) {
+            int[] result = new int[reversePostOrder.size()];
+            for (int i = 0; i < result.length; i++)
+                result[i] = reversePostOrder.pop();
+            return result;
+        }
+        return new int[]{};
+    }
+
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        IntStream.range(0, numCourses).forEach(i -> adjMap.put(i, new LinkedList<>()));
         // Construct adjMap
         for (int[] prerequisite: prerequisites) {
             int w = prerequisite[0];
             int v = prerequisite[1];
-            adjMap.putIfAbsent(v, new LinkedList<>());
-            adjMap.putIfAbsent(w, new LinkedList<>());
             adjMap.get(v).add(w);
         }
 
         // Find if cycle exists in the graph.
-        // Since this is a directed graph the check has to be done starting all verticies
-
-
-        Set<Integer> visited = new HashSet<>();
-        Set<Integer> onStack = new HashSet<>();
+        // Since there can be multiple disjointed graphs, the check has to be done starting all verticies
         for (int start : adjMap.keySet()) {
-            if (!visited.contains(start) && hasCycle(start, adjMap, visited, onStack))
+            if (!visited.contains(start) && hasCycle(start))
                 return false;
         }
         return true;
     }
 
-    public boolean hasCycle(int v, Map<Integer, List<Integer>> adjMap, Set<Integer> visited, Set<Integer> onStack) {
+    public boolean hasCycle(int v) {
         visited.add(v);
         onStack.add(v); // CRUX
         for (int w : adjMap.get(v)) {
             if (onStack.contains(w)) // CRUX
                 return true;
-            if (!visited.contains(w) && hasCycle(w, adjMap, visited, onStack))
+            if (!visited.contains(w) && hasCycle(w))
                 return true;
         }
         onStack.remove(v); // CRUX
+        reversePostOrder.push(v); // CRUx
         return false;
     }
 }
--- src/test/java/org/am/graphs/CourseScheduleTest.java
+++ src/test/java/org/am/graphs/CourseScheduleTest.java
@@ -2,16 +2,20 @@ package org.am.graphs;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseScheduleTest {
 
     @Test
     void canFinish() {
+        assertTrue(new CourseSchedule().canFinish(2, new int[][]{{1, 0}}));
+        assertArrayEquals(new int[]{0, 1}, new CourseSchedule().findOrder(2, new int[][]{{1, 0}}));
+
         assertFalse(new CourseSchedule().canFinish(2, new int[][]{{0, 1}, {1, 0}}));
+        assertArrayEquals(new int[]{}, new CourseSchedule().findOrder(2, new int[][]{{0, 1}, {1, 0}}));
 
         assertTrue(new CourseSchedule().canFinish(4, new int[][]{{1, 4}, {2, 4}, {3, 1}, {3, 2}}));
-        assertTrue(new CourseSchedule().canFinish(2, new int[][]{{1, 0}}));
         assertTrue(new CourseSchedule().canFinish(2, new int[][]{{0, 1}, {1, 2}}));
         assertTrue(new CourseSchedule().canFinish(2, new int[][]{{0, 1}, {1, 2}, {3, 4}}));
     }
