e76020d5489c48b576e6e2c3805f1f9591a83920
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-06-17 15:31:01 +0530

    Cleaner solutions

--- src/main/java/org/am/sliding/window/MinWindow.java
+++ src/main/java/org/am/sliding/window/MinWindow.java
@@ -3,47 +3,42 @@ package org.am.sliding.window;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * https://leetcode.com/problems/minimum-window-substring/description/
+ */
 public class MinWindow {
 
     public String minWindow(String s, String t) {
+        Map<Character, Integer> sCount = new HashMap<>();
+        Map<Character, Integer> tCount = charCount(t);
+        String min = s + t;
 
-        Map<Character, Integer> winCount = new HashMap<>();
-        Map<Character, Integer> tCount = new HashMap<>();
-        for (Character c : t.toCharArray()) {
-            tCount.put(c, tCount.getOrDefault(c, 0) + 1);
-        }
-
-        String minWindow = "";
-        int need = tCount.size();
-        int have = 0;
-
-        for (int r = 0, l = 0; r < s.length(); r++) {
-            char rChar = s.charAt(r);
-            if (!tCount.containsKey(rChar))
-                continue;
-
-            winCount.put(rChar, winCount.getOrDefault(rChar, 0) + 1);
-            if (winCount.get(rChar).equals(tCount.get(rChar)))
-                have++;
-            for (;have == need; l++) {
-                minWindow = updateMinWindow(minWindow, s, l, r);
-                char lChar = s.charAt(l);
-                if (!tCount.containsKey(lChar))
-                    continue;
-                winCount.computeIfPresent(lChar, (k, v) -> v - 1);
-                if (winCount.get(lChar) < tCount.get(lChar)) {
-                    have--;
-                }
+        for (int l = 0, r = 0; r < s.length(); r++) {
+            sCount.compute(s.charAt(r), (k, v) -> v == null ? 1 : v + 1);
+            while (superset(sCount, tCount)) {
+                if (min.length() > r - l + 1)
+                    min = s.substring(l, r + 1);
+                sCount.compute(s.charAt(l), (k, v) -> v == 1 ? null : v - 1);
+                l++;
             }
         }
-        return minWindow;
+        return min.equals(s + t) ? "" : min;
     }
 
-    private String updateMinWindow(String minWindow, String s, int l, int r) {
-        String curWindow = s.substring(l, r + 1);
-        if (minWindow.isEmpty()) {
-            return curWindow;
+    private boolean superset(Map<Character, Integer> sCount, Map<Character, Integer> tCount) {
+        for (Map.Entry<Character, Integer> entry : tCount.entrySet()) {
+            if (!sCount.containsKey(entry.getKey()))
+                return false;
+            if (sCount.get(entry.getKey()) < entry.getValue())
+                return false;
         }
-        return curWindow.length() < minWindow.length() ? curWindow : minWindow;
+        return true;
+    }
+
+    private Map<Character, Integer> charCount(String s) {
+        Map<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < s.length(); i++)
+            count.compute(s.charAt(i), (k, v) -> v == null ? 1 : v + 1);
+        return count;
     }
 }
