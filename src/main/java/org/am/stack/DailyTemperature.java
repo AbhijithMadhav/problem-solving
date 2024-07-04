5611e158c12eb4fec34c7e17f8a239fd5e7d2264
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-07-04 18:36:17 +0530

    Improved solution

--- src/main/java/org/am/stack/DailyTemperature.java
+++ src/main/java/org/am/stack/DailyTemperature.java
@@ -1,7 +1,7 @@
 package org.am.stack;
 
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Given an array of integers temperatures represents the daily temperatures,
@@ -10,31 +10,29 @@ import java.util.List;
  * keep answer[i] == 0 instead.
  */
 public class DailyTemperature {
+
     public int[] dailyTemperatures(int[] temperatures) {
-
-        TempAndIndex[] tempAndIndices = new TempAndIndex[temperatures.length];
-        int[] result = new int[temperatures.length];
-
-        for (int i = 0; i < temperatures.length; i++)
-            tempAndIndices[i] = new TempAndIndex(temperatures[i], i);
-
-        List<TempAndIndex> stack = new LinkedList<>();
+        Deque<Integer> tempStack = new LinkedList<>();
+        Deque<Integer> indexStack = new LinkedList<>();
+        int[] answer = new int[temperatures.length];
 
         // Crux is to recognize that nDay of a temperature can be calculated only once the later higher temperature is encountered
         // Now usage of a stack to store the temperature follows
-        for (int i = 0; i < tempAndIndices.length; i++) {
-            while (!stack.isEmpty() && tempAndIndices[i].temp > stack.getFirst().temp) {
-                TempAndIndex lower = stack.removeFirst();
-                result[lower.index] = i - lower.index;
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!tempStack.isEmpty() && tempStack.peek() < temperatures[i]) {
+                tempStack.pop();
+                int index = indexStack.pop();
+                answer[index] = i - index;
             }
-            stack.addFirst(tempAndIndices[i]);
+            tempStack.push(temperatures[i]);
+            indexStack.push(i);
         }
-        // not strictly necessary in the java implementation as result[] is all 0's
-        while (!stack.isEmpty())
-            result[stack.removeFirst().index] = 0;
 
-        return result;
+        while(!tempStack.isEmpty()) {
+            tempStack.pop();
+            answer[indexStack.pop()] = 0;
+        }
+
+        return answer;
     }
-
-    private record TempAndIndex(int temp, int index){ }
 }
