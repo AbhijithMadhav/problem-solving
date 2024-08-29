8de989c57b292d323dbf6c9e8ee959f3461bbe8e
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-08-29 13:26:36 +0530

    Subset generation

--- src/main/java/org/am/back/tracking/SubsetsWithDup.java
+++ src/main/java/org/am/back/tracking/SubsetsWithDup.java
@@ -1,18 +1,16 @@
 package org.am.back.tracking;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- * Given an integer array nums of unique elements, return all possible subsets(the power set).
- * The solution set must not contain duplicate subsets.
- * Return the solution in any order.
- * <a href="https://leetcode.com/problems/subsets/">...</a>
+ * Given an integer array nums that may contain duplicates, return all possible subsets(the power set).
+ * The solution set must not contain duplicate subsets. Return the solution in any order.
+ * <a href="https://leetcode.com/problems/subsets-ii/">...</a>
  */
-public class SubSet {
-
+public class SubsetsWithDup {
     List<List<Integer>> subsets = new ArrayList<>();
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
         backtrack(nums, 0, new ArrayList<>());
         return subsets;
     }
@@ -20,6 +18,8 @@ public class SubSet {
     public void backtrack(int[] nums, int index, List<Integer> currentSubset) {
         subsets.add(new ArrayList<>(currentSubset));
         for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1])
+                continue;
             currentSubset.addLast(nums[i]);
             backtrack(nums, i + 1, currentSubset);
             currentSubset.removeLast();
---
+++ src/test/java/org/am/back/tracking/SubsetsWithDupTest.java
@@ -0,0 +1,31 @@
+package org.am.back.tracking;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SubsetsWithDupTest {
+
+    @Test
+    void subsetsWithDup() {
+
+        assertThat(new SubsetsWithDup().subsetsWithDup(new int[] {1, 2, 3})).hasSameElementsAs(
+                List.of(
+                        List.of(),
+                        List.of(1), List.of(2), List.of(3),
+                        List.of(1, 2), List.of(1, 3), List.of(2, 3),
+                        List.of(1, 2, 3)
+                )
+        );
+        assertThat(new SubsetsWithDup().subsetsWithDup(new int[]{1, 2, 1, 3})).hasSameElementsAs(
+                List.of(
+                        List.of(),
+                        List.of(1), List.of(1, 1), List.of(1, 1, 2), List.of(1, 1, 2, 3), List.of(1, 1, 3), List.of(1, 2), List.of(1, 2, 3), List.of(1, 3),
+                        List.of(2), List.of(2, 3),
+                        List.of(3)
+                )
+        );
+    }
+}
