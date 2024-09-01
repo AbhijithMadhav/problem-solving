d1605341dcf9438844452d2044ded219cd08632c
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-09-01 14:46:00 +0530

    Top K

--- src/main/java/org/am/heap/TopK.java
+++ src/main/java/org/am/heap/TopK.java
@@ -1,25 +1,27 @@
 package org.am.heap;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class TopK {
 
     public int[] topKFrequent(int[] nums, int k) {
 
+        assert k > 0;
         Map<Integer, Integer> countMap = new HashMap<>();
-        for (int num : nums) { // O(n)
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-        }
 
-        // O(nlogn)
-        // Could also sort here
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue());
-        pq.addAll(countMap.entrySet());
+        // O(n)
+        Arrays.stream(nums).forEach(num -> countMap.put(num, countMap.getOrDefault(num, 0) + 1));
 
-        // O(klogn)
-        int[] result = new int[k];
-        IntStream.range(0, k).forEach(i -> result[i] = pq.remove().getKey());
-        return result;
+        // O(n log k)
+        // Could also sort here. But that would be O(n log n)
+        // Min heap
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        countMap.entrySet().forEach(entry -> {
+            pq.add(entry); // log k
+            if (pq.size() > k)
+                pq.remove(); // log k
+        });
+
+        return pq.stream().mapToInt(Map.Entry::getKey).toArray();
     }
 }
