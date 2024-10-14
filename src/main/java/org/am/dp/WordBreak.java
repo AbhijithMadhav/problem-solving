9bfc66d0cca6304de403b4fe7ef20fda8c25a1ce
Abhijith Madhav <1021302+AbhijithMadhav@users.noreply.github.com>
2024-10-14 15:01:20 +0530

    Refractoring

--- src/main/java/org/am/dp/WordBreak.java
+++ src/main/java/org/am/dp/WordBreak.java
@@ -5,10 +5,10 @@ import java.util.*;
 /**
  * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
  * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ * <a href="https://leetcode.com/problems/word-break/description/">...</a>
  */
 public class WordBreak {
 
-    private final Map<Integer, Boolean> cache = new HashMap<>();
     public boolean wordBreak(String s, List<String> wordDict) {
         return wordBreak(s, 0, new HashSet<>(wordDict));
     }
@@ -16,18 +16,21 @@ public class WordBreak {
     // Crux is to recognize and visualize the solution as a decision tree
     private boolean wordBreak(String s, int start, Set<String> wordDict) {
 
-        if (cache.containsKey(start))
-            return cache.get(start);
-
         for (int i = start; i < s.length(); i++) {
             String word = s.substring(start, i + 1);
             if (!wordDict.contains(word))
                 continue;
-            boolean isBreakable = wordBreak(s, i + 1, wordDict);
-            cache.put(start, isBreakable);
+            boolean isBreakable = cache(s, i + 1, wordDict);
             if (isBreakable)
                return true;
         }
         return start >= s.length();
+    }
+
+    private final Map<Integer, Boolean> cache = new HashMap<>();
+    private boolean cache(String s, int start, Set<String> wordDict) {
+        if (!cache.containsKey(start))
+            cache.put(start, wordBreak(s, start, wordDict));
+        return cache.get(start);
     }
 }
